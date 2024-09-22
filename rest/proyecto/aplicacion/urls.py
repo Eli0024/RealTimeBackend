6be11885a2c94acc62a0f909081d61ee60b47288
Eli0024 <eliana199972@gmail.com>
2024-09-21 19:31:57 -0500

    vistas modificadas

--- rest/proyecto/aplicacion/urls.py
+++ rest/proyecto/aplicacion/urls.py
@@ -1,20 +1,30 @@
 from django.urls import path
-from .views import (ConfigurarParametrosDetailView, MedicionesDetailView, RegistrarEncargadoDetailView, RegistrarGalponDetailView, RegistrarGranjaDetailView, UserRegisterView,ObtainAuthTokenView,RegistrarGranjaView,RegistrarGalponView,ConfigurarParametrosView,
-    MedicionesView,RegistrarEncargadoView)
-
+from .views import (
+    ConfigurarParametrosDetailView,
+    MedicionesDetailView,
+    RegistrarEncargadoDetailView,
+    RegistrarGalponDetailView,
+    RegistrarGranjaDetailView,
+    UserRegisterView,
+    ObtainAuthTokenView,
+    RegistrarGranjaView,
+    RegistrarGalponView,
+    ConfigurarParametrosView,
+    MedicionesView,
+    RegistrarEncargadoView
+)
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', ObtainAuthTokenView.as_view(), name='login'),
     path('galpones/', RegistrarGalponView.as_view(), name='galpon_list'),
-    path('galpones/<int:pk>/', RegistrarGalponDetailView.as_view(), name='galpon_list'),
+    path('galpones/<int:pk>/', RegistrarGalponDetailView.as_view(), name='galpon_detail'),
     path('granjas/', RegistrarGranjaView.as_view(), name='granja_list'),
-    path('granjas/<int:pk>/', RegistrarGranjaDetailView.as_view(), name='granja_list'),
+    path('granjas/<int:pk>/', RegistrarGranjaDetailView.as_view(), name='granja_detail'),
     path('parametros/', ConfigurarParametrosView.as_view(), name='parametro_list'),
-    path('parametros/<int:pk>/', ConfigurarParametrosDetailView.as_view(), name='granja_list'),
+    path('parametros/<int:pk>/', ConfigurarParametrosDetailView.as_view(), name='parametro_detail'),
     path('mediciones/', MedicionesView.as_view(), name='medicion_list'),
-    path('mediciones/<int:pk>/', MedicionesDetailView.as_view(), name='granja_list'),
+    path('mediciones/<int:pk>/', MedicionesDetailView.as_view(), name='medicion_detail'),
     path('encargados/', RegistrarEncargadoView.as_view(), name='encargado_list'),
-    path('encargados/<int:pk>/', RegistrarEncargadoDetailView.as_view(), name='granja_list'),
+    path('encargados/<int:pk>/', RegistrarEncargadoDetailView.as_view(), name='encargado_detail'),
 ]
-
