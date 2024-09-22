a6cecc5be7ac20d5458c78480dbb953acee2655a
SANTIAGO CONDA <yonatanpazu2@gmail.com>
2024-09-21 23:32:47 -0500

    modifique el urls.py, faltaba una coma en la url

--- rest/proyecto/aplicacion/urls.py
+++ rest/proyecto/aplicacion/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-<<<<<<< HEAD
+
 from . import views
 from .views import (ConfigurarParametrosDetailView, MedicionesDetailView, RegistrarEncargadoDetailView, RegistrarGalponDetailView, RegistrarGranjaDetailView, UserRegisterView,ObtainAuthTokenView,RegistrarGranjaView,RegistrarGalponView,ConfigurarParametrosView,
     MedicionesView,RegistrarEncargadoView)
 
-=======
+
 from .views import (
     ConfigurarParametrosDetailView,
     MedicionesDetailView,
@@ -19,7 +19,7 @@ from .views import (
     MedicionesView,
     RegistrarEncargadoView
 )
->>>>>>> 6be11885a2c94acc62a0f909081d61ee60b47288
+
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
@@ -33,10 +33,9 @@ urlpatterns = [
     path('mediciones/', MedicionesView.as_view(), name='medicion_list'),
     path('mediciones/<int:pk>/', MedicionesDetailView.as_view(), name='medicion_detail'),
     path('encargados/', RegistrarEncargadoView.as_view(), name='encargado_list'),
-<<<<<<< HEAD
     path('encargados/<int:pk>/', RegistrarEncargadoDetailView.as_view(), name='granja_list'),
-    path('mediciones/',views.registrarMediciones, name='mediciones')
-=======
-    path('encargados/<int:pk>/', RegistrarEncargadoDetailView.as_view(), name='encargado_detail'),
->>>>>>> 6be11885a2c94acc62a0f909081d61ee60b47288
+    path('mediciones/',views.registrarMediciones, name='mediciones'),
+    path('encargados/<int:pk>/', RegistrarEncargadoDetailView.as_view(), name='encargado_detail')
 ]
+
+
